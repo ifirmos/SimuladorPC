75674624077c89708cb3b17fd13dff2d6880aeb6
ifirmos <isaac.firmos@gmail.com>
2024-04-16 19:48:33 -0300

    Exclusão de Arquivos Desnecessários

--- SimuladorPC.Domain/PortaUsb.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace SimuladorPC.Domain;
-
-public class PortaUsb
-{
-    public string Versao { get; set; } // Ex: "2.0", "3.0", "3.1", "3.2", "Type-C"
-    public int Quantidade { get; set; } // Número de portas desse tipo
-}
--- SimuladorPC.Domain/SlotM2.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SimuladorPC.Domain;
-
-public class SlotM2
-{
-    public string Tamanho { get; set; } = string.Empty;
-    public bool SuportaNvme { get; set; } // True se o slot suporta NVMe, false caso contrário
-    public bool SuportaSata { get; set; }
-    public string Versao {  get; set; } = string.Empty;
-}
